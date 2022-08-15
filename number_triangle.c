7bfa053d4347336c0c6105a6dca5fe01d9e1bd08
RaamJain <105813493+RaamJain@users.noreply.github.com>
2022-08-15 22:11:57 +0530

    Add files via upload

---
+++ number_triangle.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the no. of layers");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=n;j>=i;j--)
+        {
+            if(j==i)
+            {
+            for(int k=j;k>0;k--)
+            {
+                printf("%d",k);
+            }
+            }
+            else{
+                printf(" ");
+            }
+            
+            
+        }
+        printf("\n");
+    }
+}
